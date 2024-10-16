9e858dfd653f0b076bdf633d59873a9758a54985
josesanchez79 <ironjo04@gmail.com>
2024-10-16 11:19:09 -0600

    added word counter and char counter functions, improved main and read book function

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        #print(file_contents)
-        print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = read_book(book_path)
+    counter = word_counter(text)
+    print(f"{text}, the word count is: {counter}")
+    lower_text = text.lower()
+    char_count = char_counter(lower_text)
+    print(char_count)
 
-def count_words(file_contents):
-    words = file_contents.split()
-    counter = 0
-    for word in words:
-        counter += 1
-    print(counter)
-    return counter
+def read_book(book_path):
+    with open (book_path) as f:
+        return f.read()
+    
+def word_counter(text):
+    return len(text)
 
+def char_counter(lower_text):
+    char_dict = {}
+    for char in lower_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
 
 main()
-
-book_read = main()
-count_words(book_read)
