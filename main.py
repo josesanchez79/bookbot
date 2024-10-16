3dd56b6edcc61fa1fa68e8eba9ba6829cc1701aa
josesanchez79 <ironjo04@gmail.com>
2024-10-16 12:17:32 -0600

    added sort_on and sort_char_list in order to sort list of char count to print on report

--- main.py
+++ main.py
@@ -2,25 +2,48 @@ def main():
     book_path = "books/frankenstein.txt"
     text = read_book(book_path)
     counter = word_counter(text)
-    print(f"{text}, the word count is: {counter}")
     lower_text = text.lower()
     char_count = char_counter(lower_text)
-    print(char_count)
+    char_list = char_dict_to_list(char_count)
+    char_list = sort_char_list(char_list)
+    print(f"--- Begin report of {book_path}")
+    print(f"{counter} words found in the document")
+    for char in char_list:
+        print(f"The '{char['char']}' character was found {char['count']} times")
+    
+
+    print("--- End report ---")
+
 
 def read_book(book_path):
     with open (book_path) as f:
         return f.read()
     
 def word_counter(text):
-    return len(text)
+    words = text.split()
+    return len(words)
 
 def char_counter(lower_text):
     char_dict = {}
     for char in lower_text:
-        if char in char_dict:
-            char_dict[char] += 1
-        else:
-            char_dict[char] = 1
+        if char.isalpha():
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
     return char_dict
+
+def char_dict_to_list(char_count):
+    char_list = []
+    for char, count in char_count.items():
+        char_list.append({"char": char, "count": count})
+    return char_list
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_char_list(char_list):
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
 
 main()
